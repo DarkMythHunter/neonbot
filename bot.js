aa16e95447dd55b0905c05727b2eb7cb6e4382bb
DarkMythHunter <40539187+DarkMythHunter@users.noreply.github.com>
2020-08-27 00:31:39 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'I love you') {
+            message.reply('I love you too');
+    }
+}};
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
