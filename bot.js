6892afbfe4bdf1cce636aabacdb4b2cf58b4379e
DarkMythHunter <40539187+DarkMythHunter@users.noreply.github.com>
2021-06-18 00:29:38 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,32 @@
 const Discord = require('discord.js');
-const MessageEmbed = require('discord.js');
-const client = new Discord.Client({disableEveryone: true});
-const auth = require("./auth.json");
+const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log('I am ready!');
 });
 
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
+client.on('message', message => {
+    if (message.content === 'I love you') {
+        message.reply('I love you too');
+    }
+});
 
-client.on('message', async message => { 
-    if (message.channel.type == 'dm') {      
-            const embed = new Discord.MessageEmbed()
-                .setColor('#F1C40F')
-                .setTitle(`${message.author.tag}`) 
-                .setDescription(message.content) 
-                .setFooter(`${message.author.id}`)
-                .setTimestamp();
-                client.channels.cache.get('855106161027710986').send(embed);
-        } 
-}) 
+client.on('message', message => {
+    if (message.content === 'nat') {
+        message.reply('gago ka ba');
+    }
+});
 
-client.on('message', async message => { 
-    if (message.channel.type == 'dm') {      
-            const embed = new Discord.MessageEmbed()
-                .setColor('#F1C40F')
-                .setDescription(message.content) 
-                client.channels.cache.get('855088827424309268').send(embed);
-        } 
+client.on('message', message => {
+    if (message.content === 'papy') {
+        message.reply('MARK GSAUCE FRIES');
+    } 
 });
 
 
